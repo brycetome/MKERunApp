99e64f73a6cff1230c61323a541fbc5e0e754457
Bryce Tome <brycept@hotmail.com>
2024-02-21 08:11:21 -0800

    Adding a team works

---
+++ Models/AccessServices/UserAccessService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.AccessServices
+{
+    public static class UserAccessService
+    {
+        public static async Task<ApplicationUser> LoadUser(
+            UserManager<ApplicationUser> UserManager,
+            AuthenticationStateProvider AuthenticationStateProvider)
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            var userId = UserManager.GetUserId(user);
+            var User = await UserManager.Users
+                .Include(u => u.CoachedTeams)
+                .SingleOrDefaultAsync(u => u.Id == userId)
+                ?? throw new NullReferenceException("Failed to load the user.");
+            return User;
+        }
+
+    }
+}
