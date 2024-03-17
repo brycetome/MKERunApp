c48722a5ecb6650be53e90080b50f65cfe18b000
Bryce Tome <brycept@hotmail.com>
2024-03-17 07:25:39 -0700

    Initial work and seperated out activity selector into its own component

---
+++ Models/WorkoutItem.cs
@@ -0,0 +1,11 @@
+﻿using static Models.ActivityType;
+
+namespace Models
+{
+    public class WorkoutItem
+    {
+        public int Id { get; set; }
+        public int DurationSeconds { get; set; }
+        public WorkoutType WorkoutType { get; set; }
+    }
+}
