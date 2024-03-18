543dadf15ae02befde9d93c9d0297422cff3ad58
Bryce Tome <brycept@hotmail.com>
2024-03-18 10:04:29 -0700

    Saving works

--- Models/WorkoutItem.cs
+++ Models/WorkoutItem.cs
@@ -5,7 +5,8 @@ namespace Models
     public class WorkoutItem
     {
         public int Id { get; set; }
-        public int DurationSeconds { get; set; }
+        public int Value { get; set; }
+        public string Measurement { get; set; } = "min";
         public WorkoutType WorkoutType { get; set; }
         public int Order { get; set; }
     }
