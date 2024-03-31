b0b5f33813b7a88aef244765a94302ffc167dbae
Bryce Tome <brycept@hotmail.com>
2024-03-31 18:16:23 -0500

    Color/workout type filtering

--- Models/ActivityType.cs
+++ Models/ActivityType.cs
@@ -2,7 +2,7 @@
 
 namespace Models
 {
-    public class ActivityType(ActivityType.WorkoutType type, string name, string icon, string color)
+    public class ActivityType(ActivityType.WorkoutType type, string name, string icon, string color, int order)
     {
         public enum WorkoutType
         {
@@ -23,6 +23,7 @@ namespace Models
         public string Icon => icon;
         public string Color => color;
         public WorkoutType Type => type;
+        public int Order => order;
 
         public override string ToString()
         {
@@ -39,29 +40,29 @@ namespace Models
             switch (type)
             {
                 case WorkoutType.Run:
-                    return new ActivityType(WorkoutType.Run, "Run", Icons.Material.Filled.DirectionsRun, "");
+                    return new ActivityType(WorkoutType.Run, "Run", Icons.Material.Filled.DirectionsRun, "", 1);
                 case WorkoutType.Bike:
-                    return new ActivityType(WorkoutType.Bike, "Bike", Icons.Material.Filled.DirectionsBike, Colors.Orange.Lighten1);
+                    return new ActivityType(WorkoutType.Bike, "Bike", Icons.Material.Filled.DirectionsBike, Colors.Orange.Lighten1, 4);
                 case WorkoutType.CrossTrain:
-                    return new ActivityType(WorkoutType.CrossTrain, "Cross Train", Icons.Material.Filled.DirectionsBike, Colors.Orange.Darken1);
+                    return new ActivityType(WorkoutType.CrossTrain, "Cross Train", Icons.Material.Filled.DirectionsBike, Colors.Orange.Darken1, 3);
                 case WorkoutType.Swim:
-                    return new ActivityType(WorkoutType.Swim, "Swim", Icons.Material.Filled.Pool, Colors.Cyan.Lighten4);
+                    return new ActivityType(WorkoutType.Swim, "Swim", Icons.Material.Filled.Pool, Colors.Cyan.Lighten4, 7);
                 case WorkoutType.WarmUp:
-                    return new ActivityType(WorkoutType.WarmUp, "Warm Up", Icons.Material.Filled.Whatshot, Colors.DeepOrange.Default);
+                    return new ActivityType(WorkoutType.WarmUp, "Warm Up", Icons.Material.Filled.Whatshot, Colors.DeepOrange.Default, 2);
                 case WorkoutType.CoolDown:
-                    return new ActivityType(WorkoutType.CoolDown, "Cool Down", Icons.Material.Filled.Thermostat, Colors.Cyan.Lighten3);
+                    return new ActivityType(WorkoutType.CoolDown, "Cool Down", Icons.Material.Filled.Thermostat, Colors.Cyan.Lighten3, 8);
                 case WorkoutType.Strength:
-                    return new ActivityType(WorkoutType.Strength, "Srength/Lift", Icons.Material.Filled.FitnessCenter, Colors.Blue.Default);
+                    return new ActivityType(WorkoutType.Strength, "Srength/Lift", Icons.Material.Filled.FitnessCenter, Colors.Blue.Default, 9);
                 case WorkoutType.Hills:
-                    return new ActivityType(WorkoutType.Hills, "Hills", Icons.Material.Filled.WifiChannel, Colors.DeepPurple.Lighten3);
+                    return new ActivityType(WorkoutType.Hills, "Hills", Icons.Material.Filled.WifiChannel, Colors.DeepPurple.Default, 10);
                 case WorkoutType.LongRun:
-                    return new ActivityType(WorkoutType.LongRun, "Long Run", Icons.Material.Filled.DirectionsRun, Colors.Yellow.Default);
+                    return new ActivityType(WorkoutType.LongRun, "Long Run", Icons.Material.Filled.DirectionsRun, Colors.Yellow.Default, 5);
                 case WorkoutType.Double:
-                    return new ActivityType(WorkoutType.Double, "Double", Icons.Material.Filled.DirectionsRun, Colors.LightGreen.Default);
+                    return new ActivityType(WorkoutType.Double, "Double", Icons.Material.Filled.DirectionsRun, Colors.Green.Darken1, 6);
                 default:
                     break;
             }
-            return new ActivityType(WorkoutType.None, "Activity", Icons.Material.Filled.RunCircle, "");
+            return new ActivityType(WorkoutType.None, "Activity", Icons.Material.Filled.RunCircle, "", 0);
         }
 
     }
