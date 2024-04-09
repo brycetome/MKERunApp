fb7ac27362455cc20b8458461234346f6cccfdeb
Bryce Tome <brycept@hotmail.com>
2024-04-09 12:48:49 -0500

    Migration files from last time

---
+++ ServerMigrations/Migrations/20240403201740_RecoveryField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerMigrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecoveryField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Recovery",
+                table: "Activity",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Recovery",
+                table: "Activity");
+        }
+    }
+}
